ca8c2883e2fab438d33fb3dea75d219c14fa3e8c
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-20 20:05:50 +0530

    React Routers set up

---
+++ src/components/create-todo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class CreateTodo extends Component {
+  state = {};
+  render() {
+    return <div>Create</div>;
+  }
+}
+
+export default CreateTodo;
---
+++ src/components/edit-todo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class EditTodo extends Component {
+  state = {};
+  render() {
+    return <div>Edit</div>;
+  }
+}
+
+export default EditTodo;
