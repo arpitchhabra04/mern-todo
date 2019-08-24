22f61d5d6552672cba8614b76a2aef63d0082697
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-24 15:42:51 +0530

    express Routers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 const PORT = 4000;
 
 let Todo = require("./todo.model");
@@ -16,6 +17,62 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("Mongodb database connection established successfully");
 });
+
+todoRoutes.route("/").get((req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route("/:id").get((req, res) => {
+  let id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route("/add").post((req, res) => {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.statusCode(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.statusCode(400).send("adding new todo failed");
+    });
+});
+
+todoRoutes.route("/update/:id").post((req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (!todo) {
+      res.statusCode(404).send("Todo not found");
+    } else {
+      todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_complete;
+
+      todo
+        .save()
+        .then(todo => {
+          res.json("Todo updated");
+        })
+        .catch(err => {
+          res.statusCode(404).send("update failed");
+        });
+    }
+  });
+});
+
+todoRoutes.route("/");
+
+app.use("/todos", todoRoutes);
+
 app.listen(PORT, () => {
   console.log("server Running at Port :" + PORT);
 });
